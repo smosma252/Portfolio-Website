359fdb58b14533ece6192dbf2318f81e56c8f073
smosma252 <osama_o.s@hotmail.com>
2021-10-06 16:32:41 -0400

    Added dependencies

---
+++ src/constants/constants.js
@@ -0,0 +1,32 @@
+export const projects = [
+  {
+    title: 'Personal Blog',
+    description: "Store blog posts in mongoDB Atlas and be able to post blogs to database through user-interface",
+    image: '/images/website.gif',
+    tags: ['Mongo', 'Express', 'EJS', 'Node', 'HTML/CSS'],
+    source: 'https://github.com/smosma252/Personal-Blog-FullStack/blob/master/README.md',
+    visit: 'https://github.com/smosma252/Personal-Blog-FullStack/blob/master/README.md',
+    id: 0,
+  },
+  {
+    title: 'E-Commerce',
+    description: "Created Frontend E-Commerce store and currently working on the backend. This will utilize Stripe API for purchases and also adding  mutliple features such as add-to-cart and possibly emailing accepted orders.  ",
+    image: '/images/image1.png',
+    tags: ['React', 'JavaScript'],
+    source: 'https://github.com/smosma252/E-commerce-Frontend',
+    visit: 'https://github.com/smosma252/E-commerce-Frontend',
+    id: 1,
+  },
+  {
+    title: 'Student Database Viewer',
+    description: "Built with user-interface with React to capture data from API and all students data. This web application can filter out users by first name and can collapse and expand information to view test scores.  ",
+    image: '/images/assessment2.gif',
+    tags: ['React', 'JavaScript', 'REST API'],
+    source: 'https://github.com/smosma252/Student-Database-Viewer/blob/main/README.md',
+    visit: 'https://github.com/smosma252/Student-Database-Viewer/blob/main/README.md',
+    id: 2,
+  },
+];
+
+export const TimeLineData = [
+];
